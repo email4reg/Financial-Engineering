60afc111ca90712544cdcff085fa1b14df9b816a
dass <dass@student.tut.fi>
2019-07-02 01:21:18 +0300

    new

--- timeseries_finance.R
+++ timeseries_finance.R
@@ -1,9 +1,12 @@
 library(quantmod)
 
-getSymbols(c("AAPL","MSFT","GOOG"),from = "2018-01-01",to = "2019-01-01",env = parent.frame())
+getSymbols(c("AAPL","MSFT","GOOG"),from = "2018-01-01",to = "2019-01-01",return.class = 'data.frame',env = parent.frame())
 
 #df_volume=cbind(Vo(AAPL),Vo(MSFT),Vo(GOOG))
-df_close=cbind(Cl(AAPL),Cl(MSFT),Cl(GOOG))
+df_close=as.data.frame(cbind(Cl(AAPL),Cl(MSFT),Cl(GOOG))) 
+colnames(df_close)[1]='AAPL'
+colnames(df_close)[2]='MSFT'
+colnames(df_close)[3]='GOOG'
 
 df_log=as.data.frame()
 j=ncol(df_close)
@@ -17,3 +20,12 @@ for(k in 1:j)
   }
   df_log=cbind(df_log,x[i])
 }
+
+df_log=data.frame()
+x = c()
+loop = 1:nrow(df_close[,1])
+for(i in loop)
+{
+  x[i] = (log(df_close[i,1])-log(df_close[i+1,1]))
+}
+df_log=cbind(df_log,x)
