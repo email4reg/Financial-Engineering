333a5eea433ca3a6b7aca2d1e665f7a525c1dd2b
Das <soumya.das@fiskars.com>
2019-07-08 17:34:07 +0300

    arima

--- timeseries_finance.R
+++ timeseries_finance.R
@@ -1,31 +1,19 @@
 library(quantmod)
+library(ggplot2)
+library(dplyr)
 
-getSymbols(c("AAPL","MSFT","GOOG"),from = "2018-01-01",to = "2019-01-01",return.class = 'data.frame',env = parent.frame())
 
-#df_volume=cbind(Vo(AAPL),Vo(MSFT),Vo(GOOG))
-df_close=as.data.frame(cbind(Cl(AAPL),Cl(MSFT),Cl(GOOG))) 
-colnames(df_close)[1]='AAPL'
-colnames(df_close)[2]='MSFT'
-colnames(df_close)[3]='GOOG'
+getSymbols("GOOG",from = "2012-01-01",to = Sys.Date(),return.class = 'data.frame',env = parent.frame())
 
-df_log=as.data.frame()
-j=ncol(df_close)
-for(k in 1:j)
-{
-  x = c()
-  loop = 1:nrow(df_close[,k])
-  for(i in loop)
-  {
-    x[i] = (log(df_close[i,k])-log(df_close[i+1,k]))
-  }
-  df_log=cbind(df_log,x[i])
-}
+GOOG<- tibble::rownames_to_column(GOOG, "Date")
 
-df_log=data.frame()
-x = c()
-loop = 1:nrow(df_close[,1])
-for(i in loop)
-{
-  x[i] = (log(df_close[i,1])-log(df_close[i+1,1]))
-}
-df_log=cbind(df_log,x)
+GOOG$Date=as.Date(as.character(GOOG$Date))
+
+ggplot(data=GOOG,mapping = aes(x = Date, y = GOOG.Close))+geom_line()
+
+#lag.plot(GOOG$GOOG.Close,lags = 5)
+
+cum_close=cumsum(GOOG$GOOG.Close)
+GOOG=cbind(GOOG,cum_close)
+
+ggplot(data=GOOG,mapping = aes(x = Date, y = cum_close))+geom_line()
